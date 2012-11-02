695a708d9e510f2621653c84a95bdf474cdbdede
Franz Junge <git@junge.f9.co.uk>
2012-11-02 15:41:46 +0000

    Spinner: Refactor of tests.

--- Fluqi.Tests/Spinner/SpinnerTests-Events.cs
+++ Fluqi.Tests/Spinner/SpinnerTests-Events.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Fluqi.Widget.jSlider;
+using Fluqi.Widget.jSpinner;
 using System.Web;
 using System.Web.Routing;
 using Fluqi.Tests.Mocks;
@@ -17,130 +17,105 @@ namespace Fluqi.Tests
 	public partial class Spinner_Events_Tests 
 	{
 
-		//[TestMethod]
-		//public void Slider_With_Create_EventHandler_Wired_Up_Renders_Correctly()
-		//{
-		//  // Arrange
-		//  // Arrange
-		//  var resp = new MockWriter();
-		//  Slider sldr = TestHelper.SetupSimpleSliderObject(resp);
+		[TestMethod]
+		public void Spinner_With_Create_EventHandler_Wired_Up_Renders_Correctly()
+		{
+		  // Arrange
+		  // Arrange
+		  var resp = new MockWriter();
+		  Spinner spin = TestHelper.SetupSimpleSpinnerObject(resp);
 
-		//  // only testing raw output
-		//  sldr.Rendering.Compress();
+		  // only testing raw output
+		  spin.Rendering.Compress();
 
-		//  sldr.Events
-		//    .SetCreateEvent("addToLog('Create event called');")
-		//  ;
+		  spin.Events
+		    .SetCreateEvent("addToLog('Create event called');")
+		  ;
 
-		//  TestHelper.ForceRender(sldr);
+		  TestHelper.ForceRender(spin);
 
-		//  // Act - Force output we'd see on the web page
-		//  string html = resp.Output.ToString();
+		  // Act - Force output we'd see on the web page
+		  string html = resp.Output.ToString();
 
-		//  // Assert
-		//  string expected = "create: function(event, ui) {addToLog('Create event called');}";
-		//  Assert.IsTrue(html.Contains(expected));
-		//}
+		  // Assert
+		  string expected = "create: function(event, ui) {addToLog('Create event called');}";
+		  Assert.IsTrue(html.Contains(expected));
+		}
 
-		//[TestMethod]
-		//public void Slider_With_Start_EventHandler_Wired_Up_Renders_Correctly()
-		//{
-		//  // Arrange
-		//  // Arrange
-		//  var resp = new MockWriter();
-		//  Slider sldr = TestHelper.SetupSimpleSliderObject(resp);
+		[TestMethod]
+		public void Spinner_With_Spin_EventHandler_Wired_Up_Renders_Correctly()
+		{
+		  // Arrange
+		  // Arrange
+		  var resp = new MockWriter();
+		  Spinner spin = TestHelper.SetupSimpleSpinnerObject(resp);
 
-		//  // only testing raw output
-		//  sldr.Rendering.Compress();
+		  // only testing raw output
+		  spin.Rendering.Compress();
 
-		//  sldr.Events
-		//    .SetStartEvent("addToLog('Start event called');")
-		//  ;
+		  spin.Events
+		    .SetSpinEvent("addToLog('Spin event called');")
+		  ;
 
-		//  TestHelper.ForceRender(sldr);
+		  TestHelper.ForceRender(spin);
 
-		//  // Act - Force output we'd see on the web page
-		//  string html = resp.Output.ToString();
+		  // Act - Force output we'd see on the web page
+		  string html = resp.Output.ToString();
 
-		//  // Assert
-		//  string expected = "start: function(event, ui) {addToLog('Start event called');}";
-		//  Assert.IsTrue(html.Contains(expected));
-		//}
+		  // Assert
+		  string expected = "spin: function(event, ui) {addToLog('Spin event called');}";
+		  Assert.IsTrue(html.Contains(expected));
+		}
 
-		//[TestMethod]
-		//public void Slider_With_Slide_EventHandler_Wired_Up_Renders_Correctly()
-		//{
-		//  // Arrange
-		//  // Arrange
-		//  var resp = new MockWriter();
-		//  Slider sldr = TestHelper.SetupSimpleSliderObject(resp);
+		[TestMethod]
+		public void Spinner_With_Start_EventHandler_Wired_Up_Renders_Correctly()
+		{
+		  // Arrange
+		  // Arrange
+		  var resp = new MockWriter();
+		  Spinner spin = TestHelper.SetupSimpleSpinnerObject(resp);
 
-		//  // only testing raw output
-		//  sldr.Rendering.Compress();
+		  // only testing raw output
+		  spin.Rendering.Compress();
 
-		//  sldr.Events
-		//    .SetSlideEvent("addToLog('Slide event called');")
-		//  ;
+		  spin.Events
+		    .SetStartEvent("addToLog('Start event called');")
+		  ;
 
-		//  TestHelper.ForceRender(sldr);
+		  TestHelper.ForceRender(spin);
 
-		//  // Act - Force output we'd see on the web page
-		//  string html = resp.Output.ToString();
+		  // Act - Force output we'd see on the web page
+		  string html = resp.Output.ToString();
 
-		//  // Assert
-		//  string expected = "slide: function(event, ui) {addToLog('Slide event called');}";
-		//  Assert.IsTrue(html.Contains(expected));
-		//}
+		  // Assert
+		  string expected = "start: function(event, ui) {addToLog('Start event called');}";
+		  Assert.IsTrue(html.Contains(expected));
+		}
 
-		//[TestMethod]
-		//public void Slider_With_Change_EventHandler_Wired_Up_Renders_Correctly()
-		//{
-		//  // Arrange
-		//  // Arrange
-		//  var resp = new MockWriter();
-		//  Slider sldr = TestHelper.SetupSimpleSliderObject(resp);
+		[TestMethod]
+		public void Spinner_With_Stop_EventHandler_Wired_Up_Renders_Correctly()
+		{
+		  // Arrange
+		  // Arrange
+		  var resp = new MockWriter();
+		  Spinner spin = TestHelper.SetupSimpleSpinnerObject(resp);
 
-		//  // only testing raw output
-		//  sldr.Rendering.Compress();
+		  // only testing raw output
+		  spin.Rendering.Compress();
 
-		//  sldr.Events
-		//    .SetChangeEvent("addToLog('Change event called');")
-		//  ;
+		  spin.Events
+		    .SetStopEvent("addToLog('Stop event called');")
+		  ;
 
-		//  TestHelper.ForceRender(sldr);
+		  TestHelper.ForceRender(spin);
 
-		//  // Act - Force output we'd see on the web page
-		//  string html = resp.Output.ToString();
+		  // Act - Force output we'd see on the web page
+		  string html = resp.Output.ToString();
 
-		//  // Assert
-		//  string expected = "change: function(event, ui) {addToLog('Change event called');}";
-		//  Assert.IsTrue(html.Contains(expected));
-		//}
-
-		//[TestMethod]
-		//public void Slider_With_Stop_EventHandler_Wired_Up_Renders_Correctly()
-		//{
-		//  // Arrange
-		//  // Arrange
-		//  var resp = new MockWriter();
-		//  Slider sldr = TestHelper.SetupSimpleSliderObject(resp);
-
-		//  // only testing raw output
-		//  sldr.Rendering.Compress();
-
-		//  sldr.Events
-		//    .SetStopEvent("addToLog('Stop event called');")
-		//  ;
-
-		//  TestHelper.ForceRender(sldr);
-
-		//  // Act - Force output we'd see on the web page
-		//  string html = resp.Output.ToString();
-
-		//  // Assert
-		//  string expected = "stop: function(event, ui) {addToLog('Stop event called');}";
-		//  Assert.IsTrue(html.Contains(expected));
-		//}
+		  // Assert
+		  string expected = "stop: function(event, ui) {addToLog('Stop event called');}";
+		  Assert.IsTrue(html.Contains(expected));
+		}
 
 	}
 
